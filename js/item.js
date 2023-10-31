3c47ee0117b9c3496b8bbe3e186a8c8849036bb8
Gabriel Laulhe <gabriellole@hotmail.com>
2023-10-30 23:17:24 -0300

    se modifican todas las paginas y se le agregan estilos

--- js/item.js
+++ js/item.js
@@ -3,4 +3,6 @@ const subtract = document.querySelector('#subtract');
 const quantity = document.querySelector('#quantity');
 
 add.addEventListener('click', () => quantity.value = Number(quantity.value) + 1);
-subtract.addEventListener('click', () => quantity.value = Number(quantity.value) - 1); /*   ver como hacer para que no tome el menos 1 */
+subtract.addEventListener('click', () => quantity.value = Number(quantity.value) - 1); 
+
+/*   ver como hacer para que no tome el menos 1 */
